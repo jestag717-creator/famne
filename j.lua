7ae7220f466d205cbfbfbfaaf42de6f696b6d155
jestag717-creator <jestag717@gmail.com>
2025-11-02 15:32:46 +0700

    Add files via upload

---
+++ j.lua
@@ -0,0 +1,133 @@
+local accounts = [[
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024D5CB21C5C229A0528120833B4DA41|name:leanryeyw|cbits:1536|playerAge:25|token:mrsPu7wkUpS2GLXwfqR99KG39LjvbnHi6a+CnRy9YTyAEBp0UME1QHrOZoBnbyjh9cyM5yGRYElZn06nDEHYVUVdmdnxcBPfh1azQbevY5BVzjl8gGfC/d8WgdppGG6IemHFHPUIsF8gxrH7blt+q2m8Q6o8xHWE2sQoDCchIfVVUif9bl+0ARLzF3y4QP47FkiOPTKUy5Stoj3YcQkak3f3Yngg9UHgUXehjq2NOxJT1le/s48HtQZtEX+9FXTRAkQ3mrT51xxn0slWxBdUiWEZJuds6izYPWWXIZv0M785I5GmbIeDE3LmN4BtRHKFWq9WlF8L2bCdpL1vLYiRT8pxQKfG4L327G8pgZTJNAjoldOBJdC6h2i+TzY4CTad66Ogtj2l4lGuYA35CE+pTV2Rky8R0CwF4VMnfQIPjrqErPrBV0F4CEiUWDyVKXS7Q7R2Gv8Mj0GUlAUJskpobkEI5dDBItwA4hs4uudXro1LqOl9qjM1ZANNA/EH+4db|vid:B23F7819-3D95-415B-9858-5AE19C9C910D
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024D5CAD11F6430B03D3FAAE1FF65ED4|name:tapeofdry|cbits:1536|playerAge:25|token:mrsPu7wkUpS2GLXwfqR99OdqRYDCaVVJO9gv3BUK84LU3QY5uwJtpYcAl/ReOlPpqqb05kaqnmIMBRHR110AwbqcoTgit4Ge6mmo/xBp4mWng9/XGMLjyeyhkZahf/zYVBFpBqOJW7/JB0ON5Mygur6GQk0KhtSsSHoHxS7VczxkO4DDgvXf6jLQ/hQbmWBiQ/VCqQYPnjUtGhKSMxyMkQvXtpeqnHX1a4M9uI8EFZM9lRzyNCuejlCY0LooXyBPjEEuXNOCqSVUSuZTUbZEMvI0OBcUPDbUHTGwril0SMGGXk1WDzAU73fKPDOe9x/zPKlSQjR6kImS/qoqC26GyjVb49yrnum3lRWodwzagPDPapvrG4KgKkdcNKHfbSIK37zgGt8X6Lt9tJd18AIKOJNxj3/8YGRfyPmJqE9MCN9I+d98lu/1BjNrTC/s1l6d2NvyHCHbLoPmTvt8FswMQc61+lTn/dlGrHpKvyAuX0ZHnTMO4G9Bs3uisLJhJ8g6|vid:580EA3FC-F671-4584-A637-4B5FAD9551F3
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024D5CC9ADEC8B7304BF7BD7D61C9238|name:wwdrabjam|cbits:1536|playerAge:25|token:W6fGm36Tx/z/GKYh0LGWH9zvAkDjrAzp3QhhRkW2gABEy/kPRyTiV3ayz60A/vc6YsqDkQRg8DC4Hw4jCRPrYgGt0l8btZQftNRg8Ea/2ZhwqeI/Hx7UDI6NHJgCtQE0P8cNNwTIEkHClAHTy4wVAkXLPh095o/Nm2+3Eys0CVkGn37G5t5/N6fS6O/IqTyB3exI3/vZ6GdWmJz0axJMxJZOxeM/KRL0a6bR5zO3+nG0Oh0l+0BBKPsXroJbbofHROdo03fU3GskYH4uZ5+u0UkmVLhaWhbtVBuFxKa6ruu+0t7n8CAfN/I/BwHbageTJ6zgzkxMxfZwgOsp3Deu7vgoG3lqvERaND6D8SvvfcSzpktp1Fln3VYHx13xYf9wuZRzWk6qZSAqD3QizNFUuKqSms9n8Gx0hOabqFxwv1Zga/cRszzOZm+k8gfbDIKFbKBbJEzMaXIF8NJ3nTGoDx6/1yfTRtK5jKp8XIYDSSyi6r5nlUWlI0vkKAueih6D|vid:7CC06978-54FE-470D-AD5A-E4E49B132B13
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024EBC24C3D6C06905C3B75044EAAE52|name:hsrdampay|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kWEPsUaqlWWqoNZwbbwTvtow73ahHPKd4ITaKecCthwm+i6G3j/b69jovmcauxY4v8MFgr4Hb9SFqvxypOaWLIfowpQ5HwZAKZ8lGkEjArhnLghFXyxoSwr943+uZJWv70cqjZE8Ka6OLJWAlsvRTj804fh/yZfKHMd1G+PksJ8PSjSaPcLatXltyJXESh5vERUhtTBpv/i5ThrFQ+E0Svs9f4C9vRt51TX0UJOYFjfwdcHbb7Xiu6wmP0PYaZ8jj8rviBnRn/YXHFJDN+DrkQksNTReANUPSD4+WOglJtx2kSVLVr/6BUXoauhzbjFtjFGA3laTy7p9GmUv70gQLwsVUkGnTAxYApZo7EqPSQzYyDBOVGEakthiXpgeBMCama+JMSIM4XmksZVjw97qCJUymlCoA4rPqaQGpsKO++nJ37wr5l7j0yBt809AYhaybrScSljQbsA4fXEyP3Dcvn6i5WgcEmIqWWcSjXb2/s+O|vid:12A55321-6017-4A2E-A5C0-4555456F1036
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024ECA4D5DC2D98807BD210D4B22944F|name:cstfacepin|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kY/nuf4nj6KM6tCU3Lom4Wm2aFewBW4p4sHVfVctzrDBTbQYFQl0Ky0LubF+v/U2GSe0I59jcD0ue4Nb+aMeXPPLst+ZMo6v81CFdbQQlpFPw2tRPUxUR7FF2J7J+lC0aV7fK+kL8rgS7SbZhqo9ri8zudwOLzZ1MRhTtyXMmsqgwP/peOV/kdJ+Sak/hOOsxsskkda26wP8DZ0XaLzLJqnm6dreLkTbSVSSA3fC7MGOlvjIdnj6ILkW7aIqTxK7VasxS61uHk8KYmGF/guvRMbbhDeh784JQ4+BtP1SEeR40uaBUobDu7DoCWxSVUBPzBdwimA1qsh5vI96Yfp+ugyNtb9LFM/18TrIoPwjKeK1gydbp4yOJ8vMRd0qrfgF1+2JoJoXdt8TF3a6e0lHy1CjeaUyY8CvAGC7RkjSFkLDsu7kvNHDTW/DUiX/ohwF4vKTDHhLEKqtPl64U4lYW8jJh8eLod8D1GYtG7R+c4Ny|vid:88FCF7AA-824C-42A8-BA06-1FE67D648B52
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024ECA353BB3D8210A9A11238CF1C048|name:stopqdnet|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kUYFEo5SdaQUJa/8Ha7SIMHojJYJNmQe0Y0ioPx8cTNvn42iDQX3GIYzA9wafo1M89EfLMCSgdJC6puunleDm9C90nSrlbX7AB300NSFsCE9EcL536OZNe81tEGg9gXUp/eBG0VQ0y0eybN4hVSpZQFkmx6/KvZFAYvMjZk9+k3KtJEIyIkwOTpk/TbmfsNeDx1Drlg7cM+mO1ETzZ1V8taWqLYDOvP5CaJSV2vma8VtOBFwiIaIhvSkV7fe7AY3Aw1pdVp8fc8YzLSX0GR/um70SnXiW5SPe0wRpIFnS7sUyMu+D+MYOHWGb4ku+C4caSAsStBVCAQNXXeDEVXxNc/ywtE77HcKDrpySHI1Ti59XCbkcHa1W+ng73ksNXiLHSFa99tI7Y64kTO+wSr3ZpybcKvtCMyuV+7SNyk16i7Sf2h/3DGt+ei8W3vSYNtPQmeg4UIoi+46r35r7OUGBbCFCOuO5ZzPrQfwORye9xBE|vid:57DF3061-6DE6-4C91-B9AD-05635EECC1FF
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024ECA5F18F1AAE1038FCC18E6137A01|name:lordmapag|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kbOdgvlcHUZvQOGBGnNYsbY/zJPEhvUCJNE58D5IY585o1yuLf37WyexjgfS2Ngogtc5xcKLOTQwkBX2+jL7H/m/cZ2JSvkoQEqW1uM0NB6lqcZTu/8OIntP8yjnV9AISKkmmY24MPdHB1JKKZp/2PWLarMED5X+DwMZ1ioBpOukyhz1Jqxf0qJu1OlFjXjBkMTzt5oG3j/tInOSt6gNhvXZSSm/9cORM74v/aKGngE6VVkx8iing2lEkhVkbN1AJyKfpmPMizeCJ9J62I8wP2dew9Upw2OeiKLnBGZDyQs9TndnuQ2qrY6utQSdubtUTmaoe9qTKrgwfGovJGpZ6usjvaWa6r4lX6QDmogqwTz2k2z6cgZSwQYVaI/6ps4ec/wkMl7YdFuby/U1C7bbboA9JFXi1JByWjLKk0BkAJsfYxusX2wcQYgdGa0irpAnlc0WP5W9rSx7kWWElXsGmvTDCGCSqPTc2p3rL4ZOZT/P|vid:ECEEEAEE-4E6C-4557-8A6F-062602DEFF3F
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024ECA68EE5AB747092AC07D24D45AE3|name:nullmatcz|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kZfDmMI23BVKmJEb+CFx4aKfQXIKkVOSo8T6pACUjgLVZTAx0ZWRYTEkqD4GUOH6mhlMhb77SoiPcoHwtHGv9tb8a5/qRAzbzMdXgUPH+oYuIW54vfsSBzOi+1k2DGiS9Vj/9oDyYrYuHDQcO4DpqcFmqZucbkan8ZxtG91kQ6YGlPnFH8/beiMr16Iee4IPTLkwx0yIuyp9BZ29LTaKeoQN0WC34MVH+V72KV+el7fqWUlCkpvZDTaUSqYKaDqrw4DLDwlHK25zdE7bFKW+WMBcwRymLIYyDd/Nxw8GZ6ZkrytMclr2vahgZrlJSR++LYvjQdI0dhCgzNsURM8804d48+fZIuUdFJpOAPB37XRb07CefCrfwr8iY6jtwLISppp4oy5lRLhCz9MM+QSZeUTlgz/geGmNtzXTbQWjlYXMacsaNuilN0g9wlCcUsGrUZbzDiqgFpl5JPubPgSADHdGud1k9PowZyUCOFtUExIb|vid:190755A0-CD18-42AF-8122-F959EF7C5468
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024EE66D29D191760332F95F3D44216A|name:mapzgezbin|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kRm1LYoKFRDAw/qk0yvWPRmAgPNjBM9UWgvcITXyjtYb0tbS5HI1IQH5lk75S9dRdriQyAxe/nmTXnzFkATpRQ7yS1a9s77fUExhAC6MuQJp3xAKNzu8vHlkAc7BHrVXlCWY18DvMPOI7uv/4STiytgP5aRWxmtxNQI3lN87JgnRTSubH9QnPV75lc622Cqli/vPJkeFYm9Rn1d5NrMbiuN2Se6JY1QOy8z92KVauRdrk8faP7h15lk7wx1rCS5tY4EUJj6N/G/c3JTXqAHieLmz9FJbnFBV9fYbn5b8Qon4v0wI1/EKDK6AwaL33L4oZE3wyXgqVNDuC4kVqsV1/ctp3OaE1Wxmits7RKzVr2jrHRzIPdp5Uy3D/yXkFMuFJFaeK1y1lCMfJMQFbY1ECaJdLiKFBsr1i2AhmIn4dXmzpEkTS85T7mGL3CXTmgPyfhDl2tdZBVQkSe13F7oJwqLOV8ipMiwR5cy5YfoQbfEu|vid:D33627D9-6D30-4161-8CA0-91D5326EA2D1
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024EE663E68604330581915D1E9E94EA|name:wilddrwwry|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kc+j5kfD27ZFwj2ex/domTFTmkcfZonTz3BHdPNLL8wDPujl8MLI+UPyYsAGahaYt7PDTKmeLvMKwJCpsOEJqvyRaL9zNtXpA2ec+GQdRcsEzhLfzXaDofXLsOaiX6kcBuiKPNzGtJQ86c8EcovjVSQGMzF3w0rMWU8gie7mcBiucmCV3XGpy+AroRdro81zKLfVRm2ZJIDo7yF8mThm7Rswl1Ses3P2gcPcxxseR8H9Sb9XcL7Ko+UrddsH274KMsjS6z3GEDKWmjKlOrLtpV91ahBhqezhmNBs9Ton6uEjra8RL1phZYV/Y3tm6ZWw/KPLJI48FnLsZoRoxoECkkLVp+7PW8rZV4OBlRj4DPMGvsXsuOOZsnZcMIfjFszIni37OCQRdutzJrqoFqvDXRv0bNvMeH4OpTL4wpWaHZK4VIf9lV3ES6y5La+lNs7cw37HQLyFyGy8ZQecAhEEZFwN4nglE6tJbBwX9+l+1meS|vid:00BF2997-F1CA-46DA-A5AF-B10BA6F8D405
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024EE67A11409C5104E0764F5835A721|name:ldwaxjug|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kb2TK1Ey9CY5zqyjgU1zoGTgfJ33vRYQbDJwkofROTCvLliDG/QYiCDZfbTbenuJw1hT6LTcJEgA4VkNZWzURlckdCU5QDPKz9zhj/ossQQhW4uDYFNyL3caIaoiWKBRemb1DaCtnmaGvZCPusSM58gtuUJbAtCjLLaydG9yfthth/nBEgC9WdvFr0OX55sgJyUlyyAAarNXLCkPFo6q3mVo5LTO5CaxyTA0/sxqh6oJBihRJBYocPh1Q+pOg7Ov17WHWPb0+sjJOYyPvqmcStEUrHn+F5ae9ZeDd5rQ0fn3T0z82mQGu2U970z9TxGTN8k6p8Y5PYGl4TdI2hq+dS66avc3k+aLZqL3qjDKdCIdKIQwqaYYdkUlr7Xbwwo78H0Wnhw4QDutMn+Y0AQZNMTXCRQdXUtB3PNQDwf3jFl11tZ82Gu8U2cThTA1vb/Eh0gz+pZK5iEffcuJowo7y8O019/uZQffdKUTVWhgePCT|vid:6839CC22-B859-4F4B-BEFA-EE38DA541D15
+mac:02:00:00:00:00:00|wk:NONE0|platform:1|rid:024EE688D35478260627DF63C18A5D00|name:ixhitwet|cbits:1536|playerAge:25|token:0jU4gruomk+4aWrYs5s/kf05rLwPFKLw7fR3Van77WaSeiXbOu0CsCSSpQXV2qw7EXCzrx14n/FpvoMojsvBbbOPtBfklven+82iUpTTOtcUCQyiktOL8WnaHCgYdX1WYxk9/IA5N2HM60R+QEEVUOfksPTjmvnX9YX865czoGMs9I5vEDM4VncV8cbt3MqN2o0YfgXiP5Xhgb1mO/WnM9lt9siurwbaUuSWeA5ur3vHkcKIa48Z2Jyagxp/PD9lmrKLkVd8ri7B+eKgMmYtTB1Rc0bA7zIkeyOaMi17ZqVlPxQ1feDoqk3rH2KQ8XqmmUG/dfAgK0mjsYYnL6IJr3+Ex6VsAaQlIVdLh0k/oPcBHgNd25mx6B8bCr4INdrSyxnI5VLuioBywdjaa5NUPtjd/MtGH4UXu3ypV7WMoNo/12QMYdljYWNkA+qNZI/1YrQz8RvuDW7M34EUquLKmDBuqGYPqmV2OZQY/jGVFukbZRhWP3ksV6/0AwukBY7q|vid:B1B79C26-C185-4C20-89DB-DF1AA50A46D9
+
+
+
+
+
+
+
+
+]]
+
+local use_bypass = true
+local auto_tutorial = false
+local auto_malady = false
+local auto_rotation = false
+local pack_name = "world_lock"
+local pack_price = 2000
+local platformID = 0
+
+local text_spam_grumbleteeth = {
+    "Aku akan musnahkan seluruh alien yang datang ke bumi",
+    "Aku adalah Super Hero",
+    "Akan ku hancurkan Pepper Block ini dalam 1 hit",
+    "Gass",
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+}
+
+local bots = {}
+
+local function setupBotCommon(bot)
+    bot:getConsole().enabled = true
+    bot.bypass_logon = use_bypass
+    bot.auto_ban = true
+    
+    local tutorial = bot.auto_tutorial
+    tutorial.enabled = auto_tutorial
+    tutorial.auto_quest = true
+    tutorial.set_as_home = true
+    tutorial.set_high_level = true
+    tutorial.set_random_skin = false
+    tutorial.set_random_profile = false
+    
+    local spam = bot.auto_spam
+    spam.auto_interval = true
+    spam.show_emote = false
+    spam.randomizer = false
+    spam.use_color = true
+    spam.messages = text_spam_grumbleteeth
+    spam.enabled = false
+    
+    local malady = bot.auto_malady
+    malady.auto_grumbleteeth = true
+    malady.auto_chicken_feet = true
+    malady.use_home = true
+    malady.auto_disconnect = false
+    malady.enabled = auto_malady
+    
+    local rotation = bot.rotation
+    rotation.harvest_until_level = true
+    rotation.tiles = 135296
+    bot.gem_limit = pack_price
+    bot:setPack(pack_name)
+    rotation.enabled = auto_rotation
+    
+    table.insert(bots, bot)
+end
+
+local function processAccount(account)
+    account = account:match("^%s*(.-)%s*$")
+    
+    if account == "" then return end
+    
+    local bot
+    
+    local email, sisa = account:match("([^|]+)|(.+)")
+    if email and sisa then
+        local mac, remaining = sisa:match("([A-Fa-f0-9][A-Fa-f0-9]:[A-Fa-f0-9][A-Fa-f0-9]:[A-Fa-f0-9][A-Fa-f0-9]:[A-Fa-f0-9][A-Fa-f0-9]:[A-Fa-f0-9][A-Fa-f0-9]:[A-Fa-f0-9][A-Fa-f0-9]):(.+)")
+        
+        if mac and remaining then
+            local rid, wk, ltoken = remaining:match("([A-Fa-f0-9]+):([A-Fa-f0-9]+):(.+)")
+            
+            if rid and wk and ltoken then
+                
+                local details = {
+                    ["name"] = ltoken,
+                    ["rid"] = rid,
+                    ["mac"] = mac,
+                    ["wk"] = wk,
+                    ["platform"] = platformID,
+                }
+                bot = addBot(details)
+            else
+                print("Failed to parse RID/WK/LTOKEN from: " .. remaining)
+                return
+            end
+        else
+            print("Failed to parse MAC address from: " .. sisa)
+            return
+        end
+    else
+        if account:match("^[^@]+@[^@]+%.[^@]+$") then
+            print("Adding bot with email format: " .. account)
+            bot = addBot(account, "xxxx")
+        else
+            print("Invalid account format: " .. account)
+            return
+        end
+    end
+    
+    if bot then
+        setupBotCommon(bot)
+        sleep(3)
+    end
+end
+
+print("Starting bot setup...")
+for account in accounts:gmatch("[^\n]+") do
+    processAccount(account)
+end
+
+print("Setup complete! Added " .. #bots .. " bots.")
